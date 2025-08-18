04d4df3e4837a98cce016e164881bd160786ebaa
dayecharry <124364526+dayecharry@users.noreply.github.com>
2025-08-18 10:39:05 +0000

    api

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, Owner, Pet, Doctor
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,23 +28,78 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# ------------- PET ---------
+# seleccionar todas las mascotas
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route("/pets", methods=["GET"])
+def get_all_pets():
+    pets = Pet.query.all()  # SQL --> SELECT * FROM pet
+    return jsonify([pet.serialize() for pet in pets]), 200
+
+# Seleccionar  mascota por id
+
+
+@app.route("/pet/<int:pet_id>",  methods=["GET"])
+def get_pet_id(pet_id):
+    pet = Pet.query.get(pet_id)
+    if not pet:
+        return jsonify({"error": "El id no existe"}), 404
+    return jsonify(pet.serialize()), 200
+
+# Crear una nueva mascota
+
+
+@app.route("/pet", methods=["POST"])
+def create_pet():
+    data = request.get_json()
+    name = data.get("name")
+    weight = data.get("weight")
+    race = data.get("race")
+
+    if not (name or weight or race):
+        return jsonify({"error": "Campos oblogatorios"}), 400
+
+    #clave = valor--> propiedadBD = VieneFront
+    pet = Pet(name=name, weight=weight,race=race)
+    db.session.add(pet)
+    db.session.commit()
+    return jsonify({"message": "Agregado con exito"}), 201
+
+@app.route("/pet/<int:pet_id>", methods=["PUT"])
+def modify_pet(pet_id): # pet_id --> se envia en la url
+    pet = Pet.query.get(pet_id) 
+    if not pet:
+        return jsonify({"error": "El id no existe"}), 404
+    data= request.get_json()  # se envia en el body 
+    if "name" in data:
+        pet.name = data["name"] # data.get("name")
+    if "weight" in data:
+        pet.weight = data["weight"]
+    db.session.commit()
+    return jsonify({"message": "Modificado con exito"}), 200
+
+
+@app.route("/pet/<int:pet_id>", methods=["DELETE"])
+def delete_pet(pet_id): # pet_id --> se envia en la url
+    pet = Pet.query.get(pet_id) 
+    if not pet:
+        return jsonify({"error": "El id no existe"}), 404
+    db.session.delete(pet)
+    db.session.commit()
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,61 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 
-db = SQLAlchemy()
+db = SQLAlchemy()  # crear una instancia de SQlAlchemy
 
-class User(db.Model):
+# POO programacion, orientada a objeto, clases (template), propiedades,  objetos, instancias
+
+
+class Pet(db.Model):
+    __tablename__ = "pet"
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    weight: Mapped[float] = mapped_column(nullable=False)
+    race: Mapped[str] = mapped_column(String(50), nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "weight": self.weight,
+            "race": self.race
         }
+
+
+class Doctor(db.Model):
+    __tablename__ = "doctor"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    speciality: Mapped[str] = mapped_column(
+        String(100), nullable=False, unique=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "speciality": self.speciality,
+            "name": self.name
+        }
+
+
+class Owner(db.Model):
+    __tablename__ = "owner"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+    phone: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "phone": self.phone
+        }
+
+# CRUD--> crear, leer, actualizar, eliminar (datos). 
+# guardar info de las mascotas,  info de los dueños de las mascotas, medicos  cual medico atendio a una mascota
+# Modelo medico (especialidad, id, nombre).  RELACION M..N
+# Modelo mascota (raza, peso, id, nombre, fk_Dueño) Modelo dueño(id, nombre, email, telefono) --> 1..M
